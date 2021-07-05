0925a7abef966411dc93262008254eb3570a34c1
lazynessmind <lazynessmind@gmail.com>
2021-07-05 23:50:21 +0100

    Support to cutout and use of immersive mod

--- android/src/lazy/clicker/MainActivity.java
+++ android/src/lazy/clicker/MainActivity.java
@@ -1,17 +1,22 @@
 package lazy.clicker;
 
-import android.os.Bundle;
-
-import com.badlogic.gdx.backends.android.AndroidApplication;
-import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import android.os.*;
+import android.view.*;
+import com.badlogic.gdx.backends.android.*;
 
 public class MainActivity extends AndroidApplication {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+			Window applicationWindow = getApplicationWindow();
+			WindowManager.LayoutParams attrib = applicationWindow.getAttributes();
+			attrib.layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+		}
+		
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        
+        cfg.useImmersiveMode = true;
         initialize(new Core(), cfg);
     }
 }
