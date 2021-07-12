bd3dcbceacadf79514cec5253cec053bd39835bc
lazynessmind <lazynessmind@gmail.com>
2021-07-12 13:30:12 +0100

    Fix Sound position

--- core/src/lazy/clicker/screen/PlayScreen.java
+++ core/src/lazy/clicker/screen/PlayScreen.java
@@ -6,84 +6,150 @@ import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.math.*;
 import lazy.clicker.logic.*;
 import lazy.clicker.api.*;
+import com.badlogic.gdx.audio.*;
+import lazy.clicker.*;
+import com.badlogic.gdx.graphics.g2d.freetype.*;
+import com.badlogic.gdx.files.*;
+import java.nio.file.*;
+import com.badlogic.gdx.utils.*;
+import lazy.clicker.utils.*;
+import lazy.clicker.effect.*;
+import lazy.clicker.implementation.*;
 
 public class PlayScreen extends ScreenAdapter
 {
 
 	private final SpriteBatch spriteBatch = new SpriteBatch();
+	private final SpriteBatch uiBatch = new SpriteBatch();
 	private final ShapeRenderer shapeRenderer = new ShapeRenderer();
-	private final BitmapFont fontRenderer = new BitmapFont();
-	
+	private BitmapFont fontRenderer;
+
 	private final float width = 20;
 	private final float ppu = Gdx.graphics.getWidth() / width;
 	private final float height = Gdx.graphics.getHeight() / ppu;
 
 	private String text = "";
-	
+
 	private final OrthographicCamera cam = new OrthographicCamera(width, height);
-	
+
 	private IClicker randomClicker;
+
+	private long numClicks = GameData.numClicks;
+
+	private Sound clickSound;
 	
-	private int score;
+	private FreeTypeFontGenerator generator;
 	
+	private Sprite sound = new Sprite(new Texture("soundoff.png"));
+	
+	private ParticleSystem system = new ParticleSystem();
+	
+	public PlayScreen(){
+		this.generator = new FreeTypeFontGenerator(Gdx.files.internal("font/regular.ttf"));
+		FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+		parameter.size = 36;
+		parameter.color = Color.BLACK;
+		this.fontRenderer = this.generator.generateFont(parameter);
+	}
+
 	@Override
 	public void show()
 	{
 		cam.position.set(width / 2, height / 2, 0);
 		cam.update();
-		fontRenderer.setScale(2f);
 		
 		randomClicker = Register.get("better");
-	}
+		this.clickSound = Gdx.audio.newSound(Gdx.files.internal("click.wav"));
 	
+		this.sound.setSize(2.5f, 2.5f);
+		this.sound.setPosition(cam.viewportWidth - 3, 1);
+	}
+
 	@Override
 	public void render(float delta)
 	{
 		Gdx.gl.glClearColor(1f, 1f, 0f, 1f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
+
 		this.cam.update();
-	
+		this.system.update(delta);
+		
+		if(Gdx.input.justTouched()){
+			Rectangle area = new Rectangle(this.cam.viewportWidth - 3f, 1f, 2.5f, 2.5f);
+			if(Utils.clickedArea(this.cam, area)){
+				GameData.clickSound = !GameData.clickSound;
+				GameData.save();
+			}
+		}
+		
+		this.sound.setTexture(!GameData.clickSound ? new Texture("soundoff.png"): new Texture("soundon.png"));
+		
 		this.shapeRenderer.setProjectionMatrix(this.cam.combined);
 		this.shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
 		this.clicked();
-		this.shapeRenderer.rect(cam.viewportWidth / 2 - 5, cam.viewportHeight /2 - 5, 10, 10);
+		this.shapeRenderer.rect(cam.viewportWidth / 2 - 5, cam.viewportHeight / 2 - 5, 10, 10);
+		this.shapeRenderer.setColor(GameData.clickSound ? Color.GREEN : Color.RED);
 		this.shapeRenderer.end();
-		
+
 		this.spriteBatch.begin();
-		//this.spriteBatch.setProjectionMatrix(this.cam.combined);
-		this.fontRenderer.draw(this.spriteBatch, "Score: " + score, 30, 30);
+		this.spriteBatch.setProjectionMatrix(this.cam.combined);
+		this.sound.draw(this.spriteBatch);
 		this.spriteBatch.end();
+	
+		this.uiBatch.begin();
+		this.fontRenderer.draw(this.uiBatch, "HinumClicks: " + GameData.numClicks, 30, Gdx.graphics.getHeight() - 30);
+		this.system.render(this.uiBatch);
+		this.fontRenderer.draw(this.uiBatch, "numClicks: " + numClicks, 30, Gdx.graphics.getHeight() - 70);
+		text = Gdx.input.getX() + " | " + Gdx.input.getY();
+		this.fontRenderer.draw(this.uiBatch, Gdx.graphics.getFramesPerSecond() + "", 30, 40);
+		this.uiBatch.end();
 	}
 
 	@Override
 	public void dispose()
 	{
 		this.shapeRenderer.dispose();
+		this.spriteBatch.dispose();
+		this.uiBatch.dispose();
+		this.clickSound.dispose();
+		this.fontRenderer.dispose();
+		this.generator.dispose();
 	}
-	
-	private boolean clicked(){
-		Vector3 touchPos = this.cam.unproject(new Vector3(
+
+	@Override
+	public void hide()
+	{
+		GameData.numClicks = this.numClicks;
+		GameData.save();
+	}
+
+	private boolean clicked()
+	{
+		Vector3 temp = new Vector3(
 			Gdx.input.getX(),
 			Gdx.input.getY(),
 			0f
-		));
-		
+		);
+		Vector3 touchPos = this.cam.unproject(temp);
+
 		float x = cam.position.x - cam.viewportWidth + touchPos.x;
 		float y = cam.position.y - cam.viewportHeight + touchPos.y;
-		
-		if(Gdx.input.justTouched()
-			&&x > -5
+
+		if (Gdx.input.justTouched()
+			&& x > -5
 			&& x < 5
 			&& y > -5
-			&& y < 5){
-				this.shapeRenderer.setColor(new Color(this.randomClicker.color()));
-				this.score += this.randomClicker.score();
-				return true;
+			&& y < 5)
+		{
+			this.shapeRenderer.setColor(new Color(this.randomClicker.color()));
+			this.numClicks += this.randomClicker.score();
+			if (GameData.clickSound) this.clickSound.play();
+			Vector3 proj = cam.project(new Vector3(touchPos.x, touchPos.y, 0));
+			this.system.add(new TextParticle(proj.x, proj.y, "+" + this.randomClicker.score()));
+			return true;
 		}
-			
+
 		this.shapeRenderer.setColor(Color.WHITE);
-		text = x + " " + y;
 		return false;
 	}
 }
